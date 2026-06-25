4f31f71e501eb35400a04e83fa7c53c218bed4c1
Hugo17180 <jarryhugo17@gmail.com>
2026-06-25 11:47:41 +0200

    initial commit

---
+++ terraform/dev.tfvars
@@ -0,0 +1,4 @@
+bucket_name   = "my-bucket"
+db_url        = "postgres://user:password@localhost:5432/mydb"
+instance_type = "t3.micro"
+environment   = "dev"
---
+++ terraform/variable.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" { type = string }
+variable "db_url"      { type = string }
+variable "instance_type" { type = string }
+variable "environment"   { type = string }
