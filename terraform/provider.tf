e4fde23c489994bd5c6253a5b0649061ec1f3d87
Hugo17180 <jarryhugo17@gmail.com>
2026-06-25 12:17:56 +0200

    Fix provider credentials for LocalStack

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,18 @@
 provider "aws" {
   region                      = "us-east-1"
+
+
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  s3_use_path_style           = true # Important pour LocalStack
+
+  access_key                  = "test"
+  secret_key                  = "test"
+
   endpoints {
     s3  = "http://localhost:4566"
-    ec2 = "http://localhost:4566"
     ssm = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
   }
 }
